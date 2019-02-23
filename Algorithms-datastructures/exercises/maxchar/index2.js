7da3d0669f2eb47071b31bf3c5ba4d5731a46cdf
Noe Alvarado <44933742+CeroDosUno@users.noreply.github.com>
2019-02-22 22:48:52 -0800

    Completed the max char practice problem. Moving on to the fizzbuzz problem.

--- Algorithms-datastructures/exercises/maxchar/index2.js
+++ Algorithms-datastructures/exercises/maxchar/index2.js
@@ -5,53 +5,12 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-
-function maxChar(str) {
-// use a character map
-  const charMap = {};
-  let max = 0; //store index
-  let maxChar = ''; //store char at the index
-
-  for (let char of str ){
-    if(charMap[char]) {
-      charMap[char]++;
-    }
-    else{
-      charMap[char] = 1;
-    }
-  }
-
-
-  for(let char in charMap){
-    if(charMap[char] > max){
-        max = charMap[char];
-        maxChar = char;
-    }
-  }
-
-  return maxChar;
-}
-
-module.exports = maxChar;
-
-
-
-/*
-// --- Directions
-// Given a string, return the character that is most
-// commonly used in the string.
-// --- Examples
-// maxChar("abcccccccd") === "c"
-// maxChar("apple 1231111") === "1"
-
 function maxChar(str) {
 // use a character map
 //pseudo code
 //use an object to map all the different maxChars
 //store the max key and max value in 2 variables.
 //loop until the character with highest amount is found.
-
-//Solution 2
 
 const charMap = {};
 let max = 0;
@@ -72,6 +31,9 @@ for(let char in charMap){
     max = charMap[char];
     maxChar = char;
   }
+  else {
+    console.log('Should not be here');
+  }
 }
 
 return maxChar;
@@ -79,4 +41,3 @@ return maxChar;
 }
 
 module.exports = maxChar;
-*/

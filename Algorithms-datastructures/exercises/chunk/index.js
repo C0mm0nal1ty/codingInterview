3085251cc64c950870ac517c87caed8f1a99fb24
Noe Alvarado <44933742+CeroDosUno@users.noreply.github.com>
2019-02-23 00:25:25 -0800

    working on comprehending the chunk array problem.

--- Algorithms-datastructures/exercises/chunk/index.js
+++ Algorithms-datastructures/exercises/chunk/index.js
@@ -8,6 +8,34 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+//Solution 1
+//pseudocode
+//create a super array to house subarrays
+//for each element in given array
+//retrieve the last element in superarray
+//if element doesnt exist or its length is equal to chunk size
+//push new subarray into superarray with current element
+//else push current element into subarray
+  const superarr = [];
+
+
+  for(let element of array){
+    const last = superarr[superarr.length -1];
+
+    if(!last || last.length === size){
+      superarr.push([element]);
+    }
+    else{
+      last.push(element);
+    }
+
+  }
+
+  return superarr;
+
+
+
+}
 
 module.exports = chunk;

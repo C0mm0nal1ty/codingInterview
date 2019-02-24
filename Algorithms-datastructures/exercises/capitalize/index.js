847e32eb2ce4c2d916a22a588f84f289c3311b0b
Noe Alvarado <44933742+CeroDosUno@users.noreply.github.com>
2019-02-23 23:04:51 -0800

    Editing my algorithm for capitalizing a string. Moving onto implementing the algorithm.

--- Algorithms-datastructures/exercises/capitalize/index.js
+++ Algorithms-datastructures/exercises/capitalize/index.js
@@ -7,6 +7,11 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+//slice the array
+//capitalize the first character
+//combine the array back into a string
+
+}
 
 module.exports = capitalize;

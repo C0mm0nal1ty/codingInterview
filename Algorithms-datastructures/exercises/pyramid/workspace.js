d5ed84c63edb378e53f209182e9cc6ced89cb885
Noe Alvarado <44933742+CeroDosUno@users.noreply.github.com>
2019-03-01 05:01:43 -0800

    working on solution pyramids with recursion.

--- Algorithms-datastructures/exercises/pyramid/workspace.js
+++ Algorithms-datastructures/exercises/pyramid/workspace.js
@@ -16,40 +16,12 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {
-//n = 3
-//row = 1
-//column = 3 ,0-5
-//midpoint = 2
-//level = ' ### '
-  const midpoint = Math.floor((2*n-1)/2);
-  for(let row = 0; row < n; row++){
-    let level = '';
-
-    for(let column = 0; column < 2 * n -1; column++){
-      if(midpoint - row <= column && midpoint + row >= column){
-        level += '#';
-      }
-      else{
-        level += ' ';
-      }
-    }
-
-    console.log(level);
+function pyramid(n, row = 0, level = '') {
+  if(row == n){
+    return;
   }
 
+  
 }
 
 module.exports = pyramid;
-//algorithm psuedocode
-//for 0 to n rows
-// create string levels
-//from 0 to columns
-//->if clumn should have a #
-//-->add a # to level
-//else
-//--> add a space to level
-//console log stair
-
-
-//EOF

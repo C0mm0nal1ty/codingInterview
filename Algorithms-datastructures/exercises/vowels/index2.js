215237d2c7c083b3aa54746ef45403e138165dfa
Noe Alvarado <44933742+CeroDosUno@users.noreply.github.com>
2019-03-01 05:27:38 -0800

    completed vowels problem with 3 solutions.

--- Algorithms-datastructures/exercises/vowels/index2.js
+++ Algorithms-datastructures/exercises/vowels/index2.js
@@ -8,11 +8,28 @@
 //   vowels('Why?') --> 0
 
 function vowels(str) {
-  //g is so it doesn't stop at the first match
-  //i - case insensitive. will turn char to lower
-  const matches = str.match(/[aeiou]/gi);
+  const charMap = {};
+  const vowels = /[aeiou]/;
+  let sum = 0;
+//  1. use the character map. map out each letter to the object
 
-  return matches ? matches.length : 0;
+  for(let char of str){
+    if(charMap[char]){
+      charMap[char]++;
+    }
+    else{
+      charMap[char] = 1;
+    }
+  }
+
+//2. run a loop checking for each time a vowel is found.
+  for(let char in charMap){
+    if(char.toLowerCase().match(vowels)){
+      sum += charMap[char];
+    }
+  }
+
+  return sum;
 }
 
 module.exports = vowels;
@@ -23,4 +40,6 @@ module.exports = vowels;
 1. use the character map. map out each letter to the object
 2. run a for loop checking for each time a vowel is found.
 3. add the total count and return the sum.
+
+
 */
--- Algorithms-datastructures/exercises/vowels/index3.js
+++ Algorithms-datastructures/exercises/vowels/index3.js
@@ -8,11 +8,17 @@
 //   vowels('Why?') --> 0
 
 function vowels(str) {
-  //g is so it doesn't stop at the first match
-  //i - case insensitive. will turn char to lower
-  const matches = str.match(/[aeiou]/gi);
+  const checker = "aeiou";
+  let sum = 0;
 
-  return matches ? matches.length : 0;
+
+  for(let char of str.toLowerCase()){
+    if(checker.includes(char)){
+      sum++;
+    }
+  }
+
+  return sum;
 }
 
 module.exports = vowels;
@@ -23,4 +29,6 @@ module.exports = vowels;
 1. use the character map. map out each letter to the object
 2. run a for loop checking for each time a vowel is found.
 3. add the total count and return the sum.
+
+
 */

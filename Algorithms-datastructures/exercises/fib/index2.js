a6667caaae7ccae4c30ea506684a8d75ae326165
Noe Alvarado <44933742+CeroDosUno@users.noreply.github.com>
2019-03-05 14:54:24 -0800

    practicing time complexity problem through the fibonacci sequence.

--- Algorithms-datastructures/exercises/fib/index2.js
+++ Algorithms-datastructures/exercises/fib/index2.js
@@ -1,3 +1,4 @@
+//RECURSIVE SOLUTION
 // --- Directions
 // Print out the n-th entry in the fibonacci series.
 // The fibonacci series is an ordering of numbers where
@@ -7,6 +8,9 @@
 // forms the first ten entries of the fibonacci series.
 // Example:
 //   fib(4) === 3
+//time complexity is O(2^N), due to 2 operations at each level of the tree.
+//space complexity is O(N), due to depth of the tree
+
 
 function fib(n) {
   if(n < 2){

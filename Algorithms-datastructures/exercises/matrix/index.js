53fa2fa070e49b5d863d18f14a4d1cff95eebe18
Noe Alvarado <44933742+CeroDosUno@users.noreply.github.com>
2019-03-05 04:26:43 -0800

    finished the spiral matrix. moving on to runtime complexity.

--- Algorithms-datastructures/exercises/matrix/index.js
+++ Algorithms-datastructures/exercises/matrix/index.js
@@ -16,7 +16,52 @@
 //     [10,  9,  8, 7]]
 
 function matrix(n) {
+  const results = [];
+  let count = 1;
+  let startcol = 0;
+  let endcol = n-1;
+  let startrow = 0;
+  let endrow = n-1;
 
+  for(let i = 0; i < n;i++){
+    results.push([]);
+  }
+
+
+  while(startcol<=endcol && startrow<= endrow){
+    //top row
+    for(let i = startcol; i <= endcol;i++){
+      results[startrow][i] = count;
+      count++;
+    }
+
+    startrow++;
+
+    //right column
+    for(let i = startrow; i <= endrow;i++){
+      results[i][endcol] = count;
+      count++;
+    }
+
+    endcol--;
+
+    //bottom row
+    for(let i = endcol; i >= startcol;i--){
+      results[endrow][i] = count;
+      count++;
+    }
+
+    //left column
+    endrow--;
+    for(let i = endrow; i >= startrow;i--){
+      results[i][startcol] = count;
+      count++;
+    }
+
+    startcol++;
+  }
+
+  return results;
 }
 
 module.exports = matrix;
@@ -24,3 +69,19 @@ module.exports = matrix;
 
 //EOF
 /*
+Pseudocode
+create an empty superarray called results
+create counter variable,set to 1
+create startcol = 0, end col = n-1, startrow = 0,endrow = n-1
+loop(startcol <= endcol and startrow <= endrow)
+->loop from startcol to endcol
+-->at results[startcol][i] = count
+--> count++
+->startrow++
+->loop from startrow to endrow
+-->at results[i][endcol]= count
+-->count++
+->endcol--;
+->repeat
+
+*/

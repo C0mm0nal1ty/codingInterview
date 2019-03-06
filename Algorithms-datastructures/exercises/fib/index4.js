3788712aedbd2d3054e48bd0b848e06be1b2cb8f
Noe Alvarado <44933742+CeroDosUno@users.noreply.github.com>
2019-03-06 05:38:00 -0800

    mastered memoization. similar to on-the-fly caching.

---
+++ Algorithms-datastructures/exercises/fib/index4.js
@@ -0,0 +1,26 @@
+//ITERATIVE SOLUTION
+// --- Directions
+// Print out the n-th entry in the fibonacci series.
+// The fibonacci series is an ordering of numbers where
+// each number is the sum of the preceeding two.
+// For example, the sequence
+//  [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+// forms the first ten entries of the fibonacci series.
+// Example:
+//   fib(4) === 3
+//time complexity is O(N) + O(N) = O(N)
+//space complexity is O(C)
+
+function fib(n) {
+  const result = [0, 1];
+  for (let i = 2; i <= n; i++) {
+    const a = result[i - 1];
+    const b = result[i - 2];
+
+    result.push(a + b);
+  }
+
+  return result[n];
+}
+
+module.exports = fib;
